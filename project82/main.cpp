8febc1dbadaa0c730a2aa47f7641d52de41bec8d
D4l3 <wrestlerdale@gmail.com>
2015-03-31 10:12:29 -0400

    Initial Commit

---
+++ project82/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project82
+//
+//  Created by Mike Dale on 3/31/15.
+//  Copyright (c) 2015 NU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
